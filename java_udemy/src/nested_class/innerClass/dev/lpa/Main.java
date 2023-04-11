099396c550a97baf44075c718ab7cb785069fbd9
Pankhuri Gupta <pankhurig1@gmail.com>
2023-04-11 08:56:33 +0530

    innerclass

--- java_udemy/src/nested_class/innerClass/dev/lpa/Main.java
+++ java_udemy/src/nested_class/innerClass/dev/lpa/Main.java
@@ -10,22 +10,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Employee> employees = new ArrayList<>(List.of(
-                new Employee(10001, "Ralph", 2015),
-                new Employee(10005, "Carole", 2021),
-                new Employee(10022, "Jane", 2013),
-                new Employee(13151, "Laura", 2020),
-                new Employee(10050, "Jim", 2018) ));
-
-//        var comparator = new EmployeeComparator<>();
-//        employees.sort(comparator);
-
-        employees.sort(new Employee.EmployeeComparator<>("yearStarted")
-                .reversed());
-
-        for (Employee e : employees) {
-            System.out.println(e);
-        }
+//        List<Employee> employees = new ArrayList<>(List.of(
+//                new Employee(10001, "Ralph", 2015),
+//                new Employee(10005, "Carole", 2021),
+//                new Employee(10022, "Jane", 2013),
+//                new Employee(13151, "Laura", 2020),
+//                new Employee(10050, "Jim", 2018) ));
+//
+////        var comparator = new EmployeeComparator<>();
+////        employees.sort(comparator);
+//
+//        employees.sort(new Employee.EmployeeComparator<>("yearStarted")
+//                .reversed());
+//
+//        for (Employee e : employees) {
+//            System.out.println(e);
+//        }
 
         System.out.println("Store Members");
 
